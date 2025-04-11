0a72f664901590399956af9d79005998fddc98aa
sasaSilver <al.mikhailov@innopolis.university>
2025-04-11 21:41:50 +0300

    ds

--- src/components/header/UserCenter.tsx
+++ src/components/header/UserCenter.tsx
@@ -1,6 +1,7 @@
 import { Button } from "~/components/ui/button";
 import { Bell } from "lucide-react";
 import ThemeToggle from "~/components/theme-toggle";
+import Link from "next/link";
 
 export default function() {
     return (
@@ -9,8 +10,8 @@ export default function() {
         <Button variant="ghost" size="icon" className="text-muted-foreground hover:text-foreground">
           <Bell className="h-5 w-5" />
         </Button>
-        <Button variant="secondary" className="text-muted-foreground hover:text-foreground">
-          Sign In
+        <Button asChild variant="secondary" className="text-muted-foreground hover:text-foreground">
+          <Link href="/auth/login">Sign In</Link>
         </Button>
       </div>
     );
